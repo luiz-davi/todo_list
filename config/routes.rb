df2d7a087b65d7acc5a629842a97fe8af2d79335
Luiz Davi <luizd4398@gmail.com>
2022-07-31 21:23:25 -0300

    improving style and flow

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
     patch "finished", to: "lists#finished"
     patch "enable", to: "lists#enable"
   end
-  get "finishedies", to: "lists#finishedies"
+  get "list/finishedies", to: "lists#finishedies"
+  get "list/open", to: "lists#open"
+
   root 'lists#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
